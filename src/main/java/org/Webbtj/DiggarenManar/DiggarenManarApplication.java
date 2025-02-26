62154580aecdc4e73fd1ed1920e54ed2420d9489
qqequalstears <christoffer.salomonsson@hotmail.com>
2025-02-26 13:49:27 +0100

    Skapar repository

---
+++ src/main/java/org/Webbtj/DiggarenManar/DiggarenManarApplication.java
@@ -0,0 +1,13 @@
+package org.Webbtj.DiggarenManar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiggarenManarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiggarenManarApplication.class, args);
+    }
+
+}
